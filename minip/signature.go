9e56641784e172bf99c14cd560bc9ad5b4aeba6a
lawr <lawrance.rsp@gmail.com>
2020-09-04 16:35:10 +0800

    fix check signature

--- minip/signature.go
+++ minip/signature.go
@@ -1,13 +1,15 @@
 package minip
 
 import (
+	"bytes"
 	"crypto/sha1"
 )
 
-func CheckDataSignature(sessionKey string, rawData string, signature string) bool {
+func CheckDataSignature(sessionKey string, rawData []byte, signature []byte) bool {
 	h := sha1.New()
 	h.Write([]byte(rawData))
 	h.Write([]byte(sessionKey))
 	check := h.Sum(nil)
-	return string(check) == signature
+
+	return bytes.Equal(check, signature)
 }
